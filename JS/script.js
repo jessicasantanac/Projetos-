e42fd3d3c7ae757eedfb98fce08ddfeea5744221
jessicasantanac <73998612+jessicasantanac@users.noreply.github.com>
2020-11-16 01:15:45 -0300

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+document.querySelector('#btn').addEventListener('click', ()=>{
+    let txtNumero = document.querySelector('#txtNumero').value;
+    console.log(txtNumero);
+    txtNumero = txtNumero.split(',');
+    console.log(txtNumero);
+    let maior = 0;
+    for(){}
+});
